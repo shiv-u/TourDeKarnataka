87f49e46b0970367352fd9f89ac251e3463ccaba
shiv-u <shivshankar.hc@gmail.com>
2018-07-28 09:08:29 +0530

    Added the missing images.

---
+++ app/src/main/java/com/androidexample/tourdemysore/ViewHolder.java
@@ -0,0 +1,21 @@
+package com.androidexample.tourdemysore;
+
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+/**
+ * Created by shivu on 27-07-2018.
+ */
+
+public class ViewHolder {
+
+    ArrayList<View> images = new ArrayList<>();
+    TextView place;
+    ImageView imageView;
+
+
+
+}
